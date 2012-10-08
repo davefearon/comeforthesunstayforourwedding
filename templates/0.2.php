d16cd474b6445affeaba3d43d58778d24c021319
Dave Fearon <dave.fearon@gmail.com>
2012-10-07 22:27:07 -0400

    using google jquery cdn

--- templates/0.2.php
+++ templates/0.2.php
@@ -135,7 +135,8 @@
 	<!-- Le javascript
 	================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
-	<script src="//static.comeforthesunstayforourwedding.com/js/jquery.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="//static.comeforthesunstayforourwedding.com/js/jquery.js"><\/script>')</script>
 	<script src="//static.comeforthesunstayforourwedding.com/js/bootstrap.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function() {
